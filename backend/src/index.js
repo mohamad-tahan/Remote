be4323f768f687a800ea3ae57687499794258058
mohamad-tahan <mohamadtahan880@gmail.com>
2022-07-05 15:36:22 +0300

    Added route for login api

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,9 @@
 const { Router } = require('express');
-const {register } = require('./user/controller/user');
+const {register,login } = require('./user/controller/user');
 const router = Router();
 
 router.post('/auth/register', register);
+router.post('/auth/login', login);
+
 
 module.exports = router;
