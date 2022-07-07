4008d101a06d49f251ea502e6e741994fee5eac0
mohamad-tahan <mohamadtahan880@gmail.com>
2022-07-07 21:14:10 +0300

    Backend: Added routes to addFiles and addLanguages

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,7 @@
 const { Router } = require('express');
 const {register,login,getUser } = require('./user/controller/user');
+const{addFiles} = require('./file/controller/file');
+const{addLanguages} = require('./language/controller/language')
 const router = Router();
 const userMiddleware = require('../middleware/userMiddleware')
 
@@ -7,5 +9,8 @@ router.post('/auth/register', register);
 router.post('/auth/login', login);
 router.get('/auth/getUser', userMiddleware(), (req, res) => getUser(req, res));
 
+router.post('/auth/addFiles', userMiddleware(), addFiles);
+
+router.post('/auth/addLanguages',addLanguages);
 
 module.exports = router;
