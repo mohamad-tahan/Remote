30a96fb28240344cbfdf875d57532f6bd1125751
mohamad-tahan <mohamadtahan880@gmail.com>
2022-07-07 20:54:27 +0300

    Backend: implemented user_functions

---
+++ backend/src/user_functions.js
@@ -0,0 +1,46 @@
+const User = require('../model/User');
+async function addUser(body, hashPassword) {
+    const {
+      name,
+      username,
+      email,
+      user_type = 0,
+      
+    } = body;
+  
+    const user = new User({
+      name,
+      username,
+      email,
+      password: hashPassword,
+      user_type,
+      
+    });
+  
+    return await user.save();
+  }
+
+  async function getUserByEmail(email) {
+    return await User.findOne({email});
+  }
+
+  async function getUserById(id) {
+    return await User.findById(id).populate("files");
+  }
+
+  async function getUsers() {
+    return await User.find();
+  }
+
+  async function getById(id) {
+    return await User.findById(id);
+  }
+
+  module.exports = {
+    addUser,
+    getUserByEmail,
+    getUserById,
+    getUsers,
+    getById,
+    
+  }
