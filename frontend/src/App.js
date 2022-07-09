6d2a8fb40bcd940cd8fce7febea285b9c8dfebed
mohamad-tahan <mohamadtahan880@gmail.com>
2022-07-09 12:39:08 +0300

    Frontend: Changed path of loginPage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route} from "react-router-dom";
-import Login from "./components/Login";
+import Login from "./components/pages/LoginPage/Login";
 import { Toaster } from 'react-hot-toast';
 
 function App() {
