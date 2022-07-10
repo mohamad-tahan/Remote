f5d6e8d1812e36b34e6057cf5ea150619bbfc85b
mohamad-tahan <mohamadtahan880@gmail.com>
2022-07-10 17:03:58 +0300

    Frontend: Created sidebar component

---
+++ frontend/src/components/pages/Ide/Sidebar.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import './Sidebar.css'
+import {FaBars,FaUserAlt}from "react-icons/fa";
+import { NavLink } from 'react-router-dom';
+import {AiFillHome, AiOutlinePlus} from "react-icons/ai";
+
+const Sidebar = ({children}) => {
+    const[isOpen ,setIsOpen] = useState(false);
+    const toggle = () => setIsOpen (!isOpen);
+
+    const menuItem=[
+        {
+            path:"/home",
+            name:"Home",
+            icon:<AiFillHome/>
+        },
+        {
+            path:"/profile",
+            name:"New Remote",
+            icon:<AiOutlinePlus/>
+        },
+        {
+            path:"/profile",
+            name:"Profile",
+            icon:<FaUserAlt/>
+        },
+     
+       
+    ]
+    return (
+        <div className="container">
+           <div style={{width: isOpen ? "250px" : "50px"}} className="sidebar">
+               <div className="top_section">
+                   <h1 style={{display: isOpen ? "block" : "none"}} className="username">@username</h1>
+                   <div style={{marginLeft: isOpen ? "50px" : "0px"}} className="bars">
+                       <FaBars onClick={toggle}/>
+                   </div>
+               </div>
+               {
+                   menuItem.map((item, index)=>(
+                       <NavLink to={item.path} key={index} className="link" activeclassName="active">
+                           <div className="icon">{item.icon}</div>
+                           <div style={{display: isOpen ? "block" : "none"}} className="link_text">{item.name}</div>
+                       </NavLink>
+                   ))
+               }
+           </div>
+           <main>{children} </main>
+        </div>
+    );
+};
+
+export default Sidebar;
