3a54d3d761812cf22d4c8f8d285582a611fb9f67
mohamad-tahan <mohamadtahan880@gmail.com>
2022-07-14 01:30:33 +0300

    Backend: Added route for getLanguages

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,7 @@
 const { Router } = require('express');
 const {register,login,getUser,updateUser, removeUser } = require('./user/controller/user');
-const{addFiles,getFilesbyId,updateFile, removeFile} = require('./file/controller/file');
-const{addLanguages} = require('./language/controller/language')
+const{addFiles,getFilesbyId,updateFile, removeFile,saveFile} = require('./file/controller/file');
+const{addLanguages,getLanguages} = require('./language/controller/language')
 const router = Router();
 const userMiddleware = require('../middleware/userMiddleware')
 
@@ -11,7 +11,8 @@ router.get('/auth/getUser', userMiddleware(), (req, res) => getUser(req, res));
 
 router.post('/auth/addFiles', userMiddleware(), addFiles);
 
-router.post('/auth/addLanguages',userMiddleware(), addLanguages);
+router.post('/auth/addLanguages', addLanguages);
+router.get('/auth/getLanguages',getLanguages );
 
 router.get('/auth/getFilesbyId', userMiddleware(), getFilesbyId);
 
