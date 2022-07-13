d5185d3f79a37500541e94000775df96316cfafb
mohamad-tahan <mohamadtahan880@gmail.com>
2022-07-14 01:27:44 +0300

    Frontend: Used monaco editor

--- frontend/src/components/pages/Ide/Remote.js
+++ frontend/src/components/pages/Ide/Remote.js
@@ -4,9 +4,11 @@ import "./Remote.css";
 import { VscRunAll } from "react-icons/vsc";
 import { BsSaveFill } from "react-icons/bs";
 import { BsFillSunFill } from "react-icons/bs";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
-import axios from 'axios';
+import axios from "axios";
+import Spinner from "../../../Spinner/Spinner";
+import Editor, { DiffEditor, useMonaco, loader } from "@monaco-editor/react";
 
 const Remote = () => {
   const [isLight, setIsLight] = useState(false);
@@ -14,121 +16,131 @@ const Remote = () => {
   const [input, setInput] = useState("");
   const [output, setOutput] = useState(null);
 
-    console.log(output);
-            
-    const handleRun = () => {
-      
-      const data = new FormData(); 
-      data.append("source_code", btoa(code)); //btoa encode the code wriiten by the user 
-      data.append("language_id",71);
-      data.append("stdin",btoa(input)) //stdin is the standard input of the user
-     axios({
+  const [language, setLanguage] = useState([]);
+  const [languageId, setLanguageId] = useState(71); //71 is id of python
+
+  const [spin, setSpin] = useState(false);
+
+  console.log("languages  ", language);
+  console.log(languageId);
+
+  console.log(output);
+  console.log(code);
+
+  const handleRun = () => {
+    setSpin(true);
+
+    const data = new FormData();
+    data.append("source_code", btoa(code)); //btoa encode the code wriiten by the user
+    data.append("language_id", languageId);
+    data.append("stdin", btoa(input)); //stdin is the standard input of the user
+    axios({
       method: "POST",
       url: "https://judge0-ce.p.rapidapi.com/submissions",
       params: { base64_encoded: "true", fields: "*" },
       headers: {
         "content-type": "application/json",
         "Content-Type": "application/json",
-        'X-RapidAPI-Key': 'c1619b850amshcd9170131f254aep18028fjsn380bac32c1bf',
-        'X-RapidAPI-Host': 'judge0-ce.p.rapidapi.com'
+        "X-RapidAPI-Key": "c1619b850amshcd9170131f254aep18028fjsn380bac32c1bf",
+        "X-RapidAPI-Host": "judge0-ce.p.rapidapi.com",
       },
       data: data,
-
-     }).then(function (response) {
-      console.log(response);
-          const token = response.data.token;
-          getResponse(token);
-        })
-        .catch((err) => {
-          let error = err.response ? err.response.data : err;
-          // get error status
-          let status = err.response.status;
-          // console.log("status", status);
-          if (status === 429) {
-            console.log("Freemium ended: ", status);
-          }
-          
-          console.log("Catch: ", error);
-        });
-    
-    }
-    const getResponse = async (token) => {
-      const res = {
-        method: "GET",
-        url: "https://judge0-ce.p.rapidapi.com/submissions/" + token,
-        params: { base64_encoded: "true", fields: "*" },
-        headers: {
-          "content-type": "application/json",
-          "Content-Type": "application/json",
-          'X-RapidAPI-Key': 'c1619b850amshcd9170131f254aep18028fjsn380bac32c1bf',
-          'X-RapidAPI-Host': 'judge0-ce.p.rapidapi.com'
-        },
-      };
-      try {
-        let response = await axios.request(res);
-        let statusDescription = response.data.status.description;
-        let statusId = response.data.status.id;
-        console.log(response.data.status);
-         console.log(statusId);
-         toast(statusDescription);
-        
-      //  statusId = 1 and statusId = 2 stand for in queue and processing so they need some time to get the response
-        if (statusId === 1 || statusId === 2) {
-          // getResponse(token)
-          setTimeout(() => {getResponse(token);}, 2000);
-          return;
-
-        } else {  
-          console.log(response)
-          setOutput(response.data);
-          console.log(atob(response.data.stdout)); //atob decodes the code that we got from the response and stdout is the standard output of the code
-          return;
+    })
+      .then(function (response) {
+        console.log(response);
+        const token = response.data.token;
+        getResponse(token);
+      })
+      .catch((err) => {
+        let error = err.response ? err.response.data : err;
+        // get error status
+        let status = err.response.status;
+        // console.log("status", status);
+        if (status === 429) {
+          console.log("Freemium ended: ", status);
         }
-      } catch (err) {
-        console.log("Error: ", err);
-      }
+
+        console.log("Catch: ", error);
+      });
+  };
+  const getResponse = async (token) => {
+    const res = {
+      method: "GET",
+      url: "https://judge0-ce.p.rapidapi.com/submissions/" + token,
+      params: { base64_encoded: "true", fields: "*" },
+      headers: {
+        "content-type": "application/json",
+        "Content-Type": "application/json",
+        "X-RapidAPI-Key": "c1619b850amshcd9170131f254aep18028fjsn380bac32c1bf",
+        "X-RapidAPI-Host": "judge0-ce.p.rapidapi.com",
+      },
     };
+    try {
+      let response = await axios.request(res);
+      let statusDescription = response.data.status.description;
+      let statusId = response.data.status.id;
+      console.log(response.data.status);
+      console.log(statusId);
+      toast(statusDescription);
+
+      //  statusId = 1 and statusId = 2 stand for in queue and processing so they need some time to get the response
+      if (statusId === 1 || statusId === 2) {
+        // getResponse(token)
+        setTimeout(() => {
+          getResponse(token);
+        }, 2000);
+        return;
+      } else {
+        console.log(response);
+        setOutput(atob(response.data.stdout));
+        setSpin(false);
+        setInput(atob(response.data.stdin));
+        console.log(atob(response.data.stdout)); //atob decodes the code that we got from the response and stdout is the standard output of the code
+        return;
+      }
+    } catch (err) {
+      console.log("Error: ", err);
+    }
+  };
 
 
+  const getLanguages = async () => {
+    const res = await fetch("http://127.0.0.1:3000/api/user/auth/getLanguages");
+    const data = await res.json();
+    console.log(data);
+    setLanguage(data);
+    return data;
+  };
 
-
-
-
-
-    
-  
-
-
-
-
-
-
-
+  useEffect(() => {
+    getLanguages();
+  }, []);
 
   const changeTheme = (e) => {
-    toast.success('Theme Changed.', {
+    theme==='light'? setTheme('vs-dark') : setTheme('light');
+    toast.success("Theme Changed.", {
       style: {
-        border: '1px solid #2e2d50',
-        padding: '8px',
-        color: '#2e2d50',
+        border: "1px solid #2e2d50",
+        padding: "8px",
+        color: "#2e2d50",
       },
       iconTheme: {
-        primary: '#2e2d50',
-        secondary: '#FFFAEE',
+        primary: "#2e2d50",
+        secondary: "#FFFAEE",
       },
     });
     setIsLight(!isLight);
     console.log(e);
   };
-
-
+  const [theme, setTheme] = useState("vs-dark");
   return (
     <div className="idePage">
       <Navbar />
-      
+
+      {spin && <Spinner />}
       <div className={`ide ${isLight && " lightTheme"}`}>
         <Sidebar />
-          <div onClick={changeTheme} className={`sun ${isLight && "dark"}`}>
+        <div onClick={changeTheme} className={`sun ${isLight && "dark"}`}>
           <BsFillSunFill />
         </div>
 
@@ -138,32 +150,55 @@ const Remote = () => {
               {" "}
               <BsSaveFill />
             </div>
-            <div className={`run ${isLight && " iconDark"}`} onClick={handleRun}>
+            <div
+              className={`run ${isLight && " iconDark"}`}
+              onClick={handleRun}
+            >
               {" "}
               <VscRunAll />
             </div>
-          
+
             <div />
           </div>
 
           <p className="fileName">filename</p>
-          <textarea
-            className={`codeText ${isLight && " theme"}`}
-            spellCheck="true"
-            onChange={(e)=>setCode(e.target.value)}
+
+          <Editor
+            height="53vh"
+            width="90vh"
+            defaultLanguage="python"
+            options={{ theme:theme, lineDecorationsWidth: 0 }}
+            onChange={(e) => setCode(e)}
           />
 
           <div>
             <p className="inputCode">Input</p>
-            <textarea className={`input ${isLight && " theme"}`} 
-            onChange={(e)=>setInput(e.target.value)}/>
+
+            <Editor
+              height="20vh"
+              options={{ lineDecorationsWidth: 0 }}
+              onChange={(e) => setInput(e)}
+            />
           </div>
         </div>
 
         <div>
-      
           <p className="outputCode">Output</p>
-          <textarea className={`output ${isLight && " theme"}`} />
+
+          {
+            <div>
+              <div className="output">
+                <Editor
+                  height="76vh"
+                  width="40vh"
+                  options={{ lineNumbers: "off", lineDecorationsWidth: 0 }}
+                  value={output}
+                />
+              </div>
+
+              {}
+            </div>
+          }
         </div>
       </div>
     </div>
