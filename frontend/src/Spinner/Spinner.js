84514f0bbc20c41cd62f02fbc4ab4e93cf01b7f2
mohamad-tahan <mohamadtahan880@gmail.com>
2022-07-17 00:29:03 +0300

    Frontend: Added Spinner Component

---
+++ frontend/src/Spinner/Spinner.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import logo from '../pics/logoSpin.png'
+import './Spinner.css'
+function Spinner() {
+  return (
+    <div><img src={logo} className="rotate" width="150px" /></div>
+  )
+}
+
+export default Spinner
+
