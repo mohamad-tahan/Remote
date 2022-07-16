0bdfd8f65a5399b4825182722c2016fef46b2774
mohamad-tahan <mohamadtahan880@gmail.com>
2022-07-17 00:36:05 +0300

    Backend: Fixed files functions

--- backend/src/file/controller/file.js
+++ backend/src/file/controller/file.js
@@ -1,102 +1,96 @@
-const {addFile} = require("../../file_functions");
+const { addFile } = require("../../file_functions");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const TOKEN_SECRET = process.env.TOKEN_SECRET || "";
 
-const User = require("../../../model/User")
-const File = require("../../../model/File")
+const User = require("../../../model/User");
+const File = require("../../../model/File");
 
 const { getFilesByOwnerId } = require("../../file_functions");
 
-
 async function addFiles(req, res) {
-    try {
-      const newFile = await addFile(req.body);
-      console.log("newFile=> ", newFile);
-      const updateUser = await User.updateOne(
-       
-        {
-          _id: newFile.user_id,
+  try {
+    const newFile = await addFile(req.body);
+    console.log("newFile=> ", newFile);
+    const updateUser = await User.updateOne(
+      {
+        _id: newFile.owner_id,
+      },
+      {
+        $push: {
+          files: newFile._id,
         },
-        {
-          $push: {
-            files: newFile._id,
-          },
-        }
-      );
-      console.log("updateUser =>", updateUser);
-      return res.status(200).send(newFile); // 200
-    } catch (error) {
-      console.log(error);
-      res.status(500).send(error);
-    }
-  }
-
-  async function getFilesbyId(req, res) {
-    try {
-      if (req.query.id) {
-        const result = await File.findOne({ _id: req.query.id });
-        console.log("result of specific file of user =>", result);
-        return res.send(result);
       }
-    } catch (error) {
-      console.log(error);
-    }
+    );
+    console.log("updateUser =>", updateUser);
+    return res.status(200).send(newFile); // 200
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
   }
+}
 
-  async function updateFile(req, res) {
-    try {
-      const file = await File.findByIdAndUpdate(
-        { _id: req.query.id },
-        {
-          $set: {
-            name: req.body.name,
-            code: req.body.code
-          },
-        }
-      );
-      return res.send("File Updated");
-    } catch (error) {
-      console.log(error);
+async function getFilesbyId(req, res) {
+  try {
+    if (req.query.id) {
+      const result = await File.findOne({ _id: req.query.id });
+      console.log("result of specific file of user =>", result);
+      return res.send(result);
     }
+  } catch (error) {
+    console.log(error);
   }
+}
 
-  async function removeFile(req, res) {
-    try {
-      const file = await File.findOne({ _id: req.query.id });
-  
-      const deleteResult = await file.remove();
-  
-      await User.updateOne(
-        { _id: file.user },
-        { $pull: { files: file._id } }
-      );
-  
-      return res.send({ message: "File Removed" });
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-  async function getFilesbyUserId(req, res) {
-    try {
-  
-      if (req.query.id) {
-        const id = req.query.id;
-        const result = await getFilesByOwnerId(id);
-        console.log("Files of owner =>", result);
-        return res.send(result);
+async function updateFile(req, res) {
+  try {
+    const file = await File.findByIdAndUpdate(
+      { _id: req.query.id },
+      {
+        $set: {
+          name: req.body.name,
+          code: req.body.code,
+          language: req.body.language,
+        },
       }
-    } catch (error) {
-      console.log(error);
-    }
+    );
+    return res.send({ message: "File Updated" });
+  } catch (error) {
+    console.log(error);
   }
+}
 
-  module.exports = {
-    addFiles,
-    getFilesbyId,
-    updateFile,
-    removeFile,
-    getFilesbyUserId,
-   
+async function removeFile(req, res) {
+  try {
+    const file = await File.findOne({ _id: req.query.id });
+
+    const deleteResult = await file.remove();
+
+    await User.updateOne({ _id: file.user }, { $pull: { files: file._id } });
+
+    return res.send({ message: "File Removed" });
+  } catch (error) {
+    console.log(error);
   }
+}
+
+async function getFilesbyUserId(req, res) {
+  try {
+    if (req.query.id) {
+      const id = req.query.id;
+      const result = await getFilesByOwnerId(id);
+      console.log("Files of owner =>", result);
+      return res.send(result);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+module.exports = {
+  addFiles,
+  getFilesbyId,
+  updateFile,
+  removeFile,
+  getFilesbyUserId,
+};
