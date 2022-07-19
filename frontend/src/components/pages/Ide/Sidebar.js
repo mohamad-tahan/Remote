0377daf9527bb4db870d4ae2510f48c41db03f44
mohamad-tahan <mohamadtahan880@gmail.com>
2022-07-19 19:29:51 +0300

    Frontend: Fixed plus button in sidebar

--- frontend/src/components/pages/Ide/Sidebar.js
+++ frontend/src/components/pages/Ide/Sidebar.js
@@ -1,66 +1,76 @@
-import React, { useState } from 'react';
-import './Sidebar.css'
-import {FaBars,FaUserAlt}from "react-icons/fa";
-import { NavLink } from 'react-router-dom';
-import {AiFillHome, AiOutlinePlus} from "react-icons/ai";
-import {RiLogoutBoxLine} from "react-icons/ri";
-import PlusButton from './PlusButton';
+import React, { useState } from "react";
+import "./Sidebar.css";
+import { FaBars, FaUserAlt } from "react-icons/fa";
+import { NavLink } from "react-router-dom";
+import { AiFillHome, AiOutlinePlus } from "react-icons/ai";
+import { RiLogoutBoxLine } from "react-icons/ri";
+import PlusButton from "./PlusButton";
 
+const Sidebar = ({ setShowModel, setIsSaving }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
+  const username = localStorage.getItem("username");
 
+  const menuItem = [
+    {
+      path: "/",
+      name: "Home",
+      icon: <AiFillHome />,
+    },
+    {
+      path: "",
+      name: "New Remote",
+      icon: <PlusButton />,
+      func: setShowModel,
+    },
 
-const Sidebar = ({setShowModel , setIsSaving}) => {
-    const[isOpen ,setIsOpen] = useState(false);
-    const toggle = () => setIsOpen (!isOpen);
-    const username = localStorage.getItem("username")
+    {
+      path: "/profile",
+      name: "Profile",
+      icon: <FaUserAlt />,
+    },
+    {
+      path: "/logout",
+      name: "Log Out",
+      icon: <RiLogoutBoxLine />,
+    },
+  ];
 
-    const menuItem=[
-        {
-            path:"/",
-            name:"Home",
-            icon:<AiFillHome/>
-        },
-        {
-            path:"",
-            name:"New Remote",
-            icon:<PlusButton handlePlusClick={setShowModel}/>
-        },
-      
-        {
-            path:"/profile",
-            name:"Profile",
-            icon:<FaUserAlt/>
-        },
-        {
-            path:"/logout",
-            name:"Log Out",
-            icon:<RiLogoutBoxLine/>
-        },
-             
-    ]
- 
-   
-    return (
-        <div className="containerr">
-           <div style={{width: isOpen ? "200px" : "50px"}} className="sidebar">
-               <div className="top_section">
-                   <h1 style={{display: isOpen ? "block" : "none"}} className="username">@{username}</h1>
-                   <div style={{marginLeft: isOpen ? "50px" : "0px"}} className="bars">
-                       <FaBars onClick={toggle}/>
-                   </div>
-               </div>
-               <br/>
-               {
-                   menuItem.map((item, index)=>(
-                       <NavLink to={item.path} key={index} className="link" activeclassName="active">
-                           <div className="icon">{item.icon}</div>
-                           <div style={{display: isOpen ? "block" : "none"}} className="link_text">{item.name}</div>
-                       </NavLink>
-                   ))
-               }
-           </div>
- 
+  return (
+    <div className="containerr">
+      <div style={{ width: isOpen ? "200px" : "50px" }} className="sidebar">
+        <div className="top_section">
+          <h1
+            style={{ display: isOpen ? "block" : "none" }}
+            className="username"
+          >
+            @{username}
+          </h1>
+          <div style={{ marginLeft: isOpen ? "50px" : "0px" }} className="bars">
+            <FaBars onClick={toggle} />
+          </div>
         </div>
-    );
+        <br />
+        {menuItem.map((item, index) => (
+          <NavLink
+            to={item.path}
+            key={index}
+            className="link"
+            activeclassName="active"
+            onClick={item.func ?? null}
+          >
+            <div className="icon">{item.icon}</div>
+            <div
+              style={{ display: isOpen ? "block" : "none" }}
+              className="link_text"
+            >
+              {item.name}
+            </div>
+          </NavLink>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
