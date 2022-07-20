7de8ba27a462e9919ab7c74231b6b2913c12c00a
mohamad-tahan <mohamadtahan880@gmail.com>
2022-07-21 02:08:00 +0300

    Frontend: Modified UserRoom

--- frontend/src/components/pages/Room/UserRoom.js
+++ frontend/src/components/pages/Room/UserRoom.js
@@ -1,61 +1,55 @@
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import Badge from '@mui/material/Badge';
-import Avatar from '@mui/material/Avatar';
-import Stack from '@mui/material/Stack';
-import logo from '../../../pics/logoSpin.png'
-import './UserRoom.css';
-
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Badge from "@mui/material/Badge";
+import Avatar from "@mui/material/Avatar";
+import Stack from "@mui/material/Stack";
+import logo from "../../../pics/logoSpin.png";
+import { deepOrange } from "@mui/material/colors";
+import "./RoomSocket.css";
 const StyledBadge = styled(Badge)(({ theme }) => ({
-  '& .MuiBadge-badge': {
-    backgroundColor: '#44b700',
-    color: '#44b700',
+  "& .MuiBadge-badge": {
+    backgroundColor: "#9904d9",
+    color: "#0716e8",
     boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
-    '&::after': {
-      position: 'absolute',
+    "&::after": {
+      position: "absolute",
       top: 0,
       left: 0,
-      width: '100%',
-      height: '100%',
-      borderRadius: '50%',
-      animation: 'ripple 1.2s infinite ease-in-out',
-      border: '1px solid currentColor',
+      width: "100%",
+      height: "100%",
+      borderRadius: "50%",
+      animation: "ripple 1s infinite ease-in-out",
+      border: "2px solid currentColor",
       content: '""',
     },
   },
-  '@keyframes ripple': {
-    '0%': {
-      transform: 'scale(.8)',
+  "@keyframes ripple": {
+    "0%": {
+      transform: "scale(.8)",
       opacity: 1,
     },
-    '100%': {
-      transform: 'scale(2.4)',
+    "100%": {
+      transform: "scale(2.4)",
       opacity: 0,
     },
   },
 }));
 
-
-export default function UserRoom({username}) {
+export default function UserRoom({ username }) {
   return (
-   
-    <div className="joinedUsers">
-    <Stack direction="row" spacing={2}>
-      <StyledBadge
-        overlap="circular"
-        anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
-        variant="dot"
-      >
-        
-        <Avatar  src={logo}  />
-       
-
-      </StyledBadge>
-      
-
- 
-    </Stack>
-    <span >{username.toUpperCase()}</span>
+    <div>
+      <div>
+        <Stack direction="row" spacing={2}>
+          <StyledBadge
+            overlap="circular"
+            anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+            variant="dot"
+          >
+            <Avatar src={logo} />
+          </StyledBadge>
+        </Stack>
+      </div>
+      <span className="username">{username.toUpperCase()}</span>
     </div>
   );
 }
