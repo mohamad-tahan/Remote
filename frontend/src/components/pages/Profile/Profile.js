2cb1c54ea93f04adbb6d7a840f97fa9f83a14224
mohamad-tahan <mohamadtahan880@gmail.com>
2022-07-22 00:18:31 +0300

    Frontend: Displayed User files

--- frontend/src/components/pages/Profile/Profile.js
+++ frontend/src/components/pages/Profile/Profile.js
@@ -1,8 +1,11 @@
-import React, {useEffect, useState} from 'react'
-import './Profile.css'
+import React, { useEffect, useState } from "react";
+import "./Profile.css";
 import { toast } from "react-hot-toast";
-import defaultPic from "../../../pics/defaultPic.png"
-
+import defaultPic from "../../../pics/defaultPic.png";
+import { TbBrandJavascript } from "react-icons/tb";
+import { FaPython } from "react-icons/fa";
+import { MdModeEditOutline } from "react-icons/md";
+import { MdDeleteForever } from "react-icons/md";
 
 function Profile() {
   const user_id = localStorage.getItem("user_id");
@@ -10,25 +13,25 @@ function Profile() {
   const [user, setUser] = useState({});
   const [profilePic, setProfilePic] = useState("");
   const [username, setUsername] = useState("");
-  const[name, setName] = useState("");
+  const [name, setName] = useState("");
+
+  const [file, setFile] = useState("");
 
   const getUser = async () => {
     const res = await fetch(
       "http://127.0.0.1:3000/api/user/auth/getUser/?id=" + user_id,
       {
-      headers: { "content-type": "application/json" ,
-      token:token},
-    }
-      );
+        headers: { "content-type": "application/json", token: token },
+      }
+    );
     const data = await res.json();
-    console.log(data);
+    // console.log(data);
 
     if (data) {
       setUser(data);
       setProfilePic(data.profilePic);
       setUsername(data.username);
       setName(data.name);
-      console.log(data);
     } else {
       toast.error("Something went wrong.");
     }
@@ -37,25 +40,59 @@ function Profile() {
   useEffect(() => {
     getUser();
   }, []);
-console.log(profilePic)
+
+  const getFiles = async () => {
+    const res = await fetch(
+      "http://127.0.0.1:3000/api/user/auth/getFilesbyUserId/?id=" + user_id,
+      {
+        headers: { "content-type": "application/json", token: token },
+      }
+    );
+    const data = await res.json();
+    console.log(data);
+
+    if (data) {
+      setFile(data);
+    } else {
+      toast.error("Something went wrong.");
+    }
+  };
+  useEffect(() => {
+    getFiles();
+  }, []);
+
   return (
-    <div class="profile">Profile
-  <div class="profile-container">
-    
-     
+    <div class="profile">
+      <div class="profile-container">
         <img src={profilePic === "" ? defaultPic : profilePic} width="200px" />
-        <br/>
+        <br />
         <span>@{username}</span>
-        <br/>
+        <br />
         <span>{name}</span>
-
       </div>
 
-      
-      
-      
+      <div class="file-container">
+        <h1>My Remotes</h1>
+        {file &&
+          file.map((i, index) => {
+            return (
+              <div className="files" key={i._id} value={i._id}>
+                {i.language === "js" ? (
+                  <TbBrandJavascript className="js" />
+                ) : (
+                  <FaPython className="py" />
+                )}{" "}
+                {i.name}
+                <div className="edit-delete">
+                  <MdModeEditOutline className="editIcon" />{" "}
+                  <MdDeleteForever className="deleteIcon" />
+                </div>
+              </div>
+            );
+          })}
       </div>
-  )
+    </div>
+  );
 }
 
-export default Profile
+export default Profile;
