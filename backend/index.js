f9d13fa5a3ecf7cc2f73bb921d9ff1cabfadab3e
mohamad-tahan <mohamadtahan880@gmail.com>
2022-07-23 21:07:31 +0300

    Backend: Increased the payload limit in nodejs

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ mongoose.connect(DB_CONNECT, (err) => {
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 app.use("/api/user", userRouter);
 // app.listen(3000, () => console.log("Server running"));
 
@@ -25,93 +25,80 @@ const io = new Server(server);
 const userSocketMap = {};
 const profilePicMap = {};
 function getConnectedUsers(roomId) {
-    return Array.from(io.sockets.adapter.rooms.get(roomId) || []).map(
-        (socketId) => {
-            return {
-                socketId,
-                username: userSocketMap[socketId],
-                profilePic: profilePicMap[socketId],
-               
-            };
-        }
-    );
+  return Array.from(io.sockets.adapter.rooms.get(roomId) || []).map(
+    (socketId) => {
+      return {
+        socketId,
+        username: userSocketMap[socketId],
+        profilePic: profilePicMap[socketId],
+      };
+    }
+  );
 }
 
+io.on("connection", (socket) => {
+  console.log("socket connected", socket.id);
 
-io.on('connection', (socket) => {
-    console.log('socket connected', socket.id);
+  socket.on("join", ({ roomId, username, profilePic }) => {
+    userSocketMap[socket.id] = username;
+    profilePicMap[socket.id] = profilePic;
 
-    socket.on("join", ({ roomId, username,profilePic }) => {
-
-        
-        userSocketMap[socket.id] = username;
-        profilePicMap[socket.id] = profilePic;
-       
-
-
-        
-
-        socket.join(roomId);
-        const users = getConnectedUsers(roomId);
-        users.forEach(({ socketId }) => {
-            io.to(socketId).emit("joined", {
-                users,
-                username,
-                profilePic,
-                socketId: socket.id,
-            });
-        });
+    socket.join(roomId);
+    const users = getConnectedUsers(roomId);
+    users.forEach(({ socketId }) => {
+      io.to(socketId).emit("joined", {
+        users,
+        username,
+        profilePic,
+        socketId: socket.id,
+      });
     });
+  });
 
-    socket.on("codeChange", ({ roomId, code }) => {
-        socket.in(roomId).emit("codeChange", { code });
+  socket.on("codeChange", ({ roomId, code }) => {
+    socket.in(roomId).emit("codeChange", { code });
+  });
+
+  socket.on("codeSocket", ({ socketId, code }) => {
+    io.to(socketId).emit("codeChange", { code });
+  });
+
+  socket.on("fileChange", ({ roomId, fileName }) => {
+    socket.in(roomId).emit("fileChange", { fileName });
+  });
+
+  socket.on("codeSocket", ({ socketId, fileName }) => {
+    io.to(socketId).emit("fileChange", { fileName });
+  });
+
+  socket.on("fileIdChange", ({ roomId, fileId }) => {
+    socket.in(roomId).emit("fileIdChange", { fileId });
+  });
+
+  socket.on("codeSocket", ({ socketId, fileId }) => {
+    io.to(socketId).emit("fileIdChange", { fileId });
+  });
+
+  socket.on("userIdChange", ({ roomId, user_id }) => {
+    socket.in(roomId).emit("userIdChange", { user_id });
+  });
+
+  socket.on("codeSocket", ({ socketId, user_id }) => {
+    io.to(socketId).emit("userIdChange", { user_id });
+  });
+
+  socket.on("disconnecting", () => {
+    const rooms = [...socket.rooms];
+    rooms.forEach((roomId) => {
+      socket.in(roomId).emit("disconnected", {
+        socketId: socket.id,
+        username: userSocketMap[socket.id],
+        profilePic: profilePicMap[socket.id],
+      });
     });
-
-    socket.on("codeSocket", ({ socketId, code }) => {
-        io.to(socketId).emit("codeChange", { code });
-    });
-
-    socket.on("fileChange", ({ roomId, fileName }) => {
-        socket.in(roomId).emit("fileChange", { fileName });
-    });
-
-    socket.on("codeSocket", ({ socketId, fileName }) => {
-        io.to(socketId).emit("fileChange", { fileName });
-    });
-
-    socket.on("fileIdChange", ({ roomId, fileId }) => {
-        socket.in(roomId).emit("fileIdChange", { fileId });
-    });
-
-    socket.on("codeSocket", ({ socketId, fileId }) => {
-        io.to(socketId).emit("fileIdChange", { fileId });
-    });
-
-    socket.on("userIdChange", ({ roomId, user_id }) => {
-        socket.in(roomId).emit("userIdChange", { user_id });
-    });
-
-    socket.on("codeSocket", ({ socketId, user_id }) => {
-        io.to(socketId).emit("userIdChange", { user_id });
-    });
-
-
-
-
-    socket.on('disconnecting', () => {
-        const rooms = [...socket.rooms];
-        rooms.forEach((roomId) => {
-            socket.in(roomId).emit("disconnected", {
-                socketId: socket.id,
-                username: userSocketMap[socket.id],
-                profilePic: profilePicMap[socket.id],
-                
-            });
-        });
-        delete userSocketMap[socket.id];
-        delete profilePicMap[socket.id];
-        socket.leave();
-    });
+    delete userSocketMap[socket.id];
+    delete profilePicMap[socket.id];
+    socket.leave();
+  });
 });
 server.listen(3000, () => console.log("Socket Server running"));
-
