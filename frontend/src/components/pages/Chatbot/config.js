6419d752ab665003352d7ca7f0549a9e4d64d87d
mohamad-tahan <mohamadtahan880@gmail.com>
2022-07-26 17:18:04 +0300

    Frontend: Inializing bot messages

--- frontend/src/components/pages/Chatbot/config.js
+++ frontend/src/components/pages/Chatbot/config.js
@@ -4,7 +4,13 @@ import { createChatBotMessage } from 'react-chatbot-kit';
 const botName = 'RemoteBot';
 
 const config = {
-  initialMessages: [createChatBotMessage(`Hi! I'm ${botName} and I'm here to help you.`)],
+  initialMessages: [createChatBotMessage(`Hi! I'm ${botName} and I'm here to help you.`),
+  createChatBotMessage(<>To start coding, you have to:<br/>
+  1. Join a Room <br/>
+  2. Create a Remote<br/>
+  3. Write your Code
+  </>),
+  createChatBotMessage(`Enjoy!`)],
   botName: botName,
   customStyles: {
     botMessageBox: {
@@ -14,7 +20,18 @@ const config = {
       backgroundColor: '#5ccc9d',
     },
   },
-
+  customComponents: {
+    // Replaces the default header
+   header: () => <div style={{ backgroundColor: 'purple', padding: "5px", borderRadius: "3px" }}>RemoteBot</div>,
+  //  // Replaces the default bot avatar
+  //  botAvatar: (props) => <MyAvatar {...props} />,
+  //  // Replaces the default bot chat message container
+  //  botChatMessage: (props) => <MyCustomChatMessage {...props} />,
+  //  // Replaces the default user icon
+  //  userAvatar: (props) => <MyCustomAvatar {...props} />,
+  //  // Replaces the default user chat message
+  //  userChatMessage: (props) => <MyCustomUserChatMessage {...props} />
+ },
 };
 
 export default config;
