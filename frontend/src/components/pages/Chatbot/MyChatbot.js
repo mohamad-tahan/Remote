0cbc3d93c2da774e4da841d4d780cd47a71811f5
mohamad-tahan <mohamadtahan880@gmail.com>
2022-07-27 00:21:47 +0300

    Frontend: Put chatbot in a draggable component

--- frontend/src/components/pages/Chatbot/MyChatbot.js
+++ frontend/src/components/pages/Chatbot/MyChatbot.js
@@ -1,18 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import Chatbot from "react-chatbot-kit";
 import "react-chatbot-kit/build/main.css";
 import config from "./config.js"
 import MessageParser from "./MessageParser.js";
 import ActionProvider from "./ActionProvider.js";
+import './Chatbot.css'
+import Draggable from 'react-draggable';
+import { GiVintageRobot } from "react-icons/gi";
+
+
+
 
 const MyChatbot = () => {
+  const[isOpen, setIsOpen] = useState(false);
+
   return (
-    <div>
-      <Chatbot
+    <div className="chat">
+      
+       <div>
+       <Draggable>
+      <GiVintageRobot className="robotIcon2" onClick={() => setIsOpen(!isOpen)} />
+      </Draggable>
+    </div>
+      <Draggable>
+        <div className={`chatbot   ${isOpen && "chatbotHide"}` }>
+          {isOpen && ( 
+             <Chatbot
         config={config}
         messageParser={MessageParser}
         actionProvider={ActionProvider}
       />
+      )}
+      
+          </div>
+          
+      </Draggable>
+     
     </div>
   );
 };
