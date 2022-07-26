cc158badce91e0aa65e4f6fc07649e48fbe4dcf5
mohamad-tahan <mohamadtahan880@gmail.com>
2022-07-27 00:19:58 +0300

    Frontend: Customized chatbot

--- frontend/src/components/pages/Chatbot/config.js
+++ frontend/src/components/pages/Chatbot/config.js
@@ -1,5 +1,8 @@
 // in config.js
 import { createChatBotMessage } from 'react-chatbot-kit';
+import MyAvatar from './MyAvatar';
+import "./Chatbot.css"
+import { FaRobot } from "react-icons/fa";
 
 const botName = 'RemoteBot';
 
@@ -14,23 +17,16 @@ const config = {
   botName: botName,
   customStyles: {
     botMessageBox: {
-      backgroundColor: '#376B7E',
+      backgroundColor: '#1e1e1e',
     },
     chatButton: {
       backgroundColor: '#5ccc9d',
     },
   },
   customComponents: {
-    // Replaces the default header
-   header: () => <div style={{ backgroundColor: 'purple', padding: "5px", borderRadius: "3px" }}>RemoteBot</div>,
-  //  // Replaces the default bot avatar
-  //  botAvatar: (props) => <MyAvatar {...props} />,
-  //  // Replaces the default bot chat message container
-  //  botChatMessage: (props) => <MyCustomChatMessage {...props} />,
-  //  // Replaces the default user icon
-  //  userAvatar: (props) => <MyCustomAvatar {...props} />,
-  //  // Replaces the default user chat message
-  //  userChatMessage: (props) => <MyCustomUserChatMessage {...props} />
+   header: () => <div className='headerBot'>RemoteBot</div>,
+   botAvatar: () => <FaRobot className='robotIcon' />,
+   userAvatar: () => <MyAvatar />,
  },
 };
 
