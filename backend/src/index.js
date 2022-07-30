11ab36138e9bc031d3050c58a51224b522ce3bdc
mohamad-tahan <mohamadtahan880@gmail.com>
2022-07-30 22:09:05 +0300

    Backend: Cleaning code

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,8 @@
 const { Router } = require('express');
-const {register,login,getUser,updateUser, removeUser } = require('./user/controller/user');
-const{addFiles,getFilesbyId,updateFile, removeFile,getFilesbyUserId, updateFileName} = require('./file/controller/file');
-const{addLanguages,getLanguages} = require('./language/controller/language')
-const{addContactUs,getContactUs} = require('./contactUs/controller/contact')
+const { register, login, getUser, updateUser, removeUser } = require('./user/controller/user');
+const { addFiles, getFilesbyId, updateFile, removeFile, getFilesbyUserId, updateFileName } = require('./file/controller/file');
+const { addLanguages, getLanguages } = require('./language/controller/language')
+const { addContactUs, getContactUs } = require('./contactUs/controller/contact')
 const router = Router();
 const userMiddleware = require('../middleware/userMiddleware')
 const Agora = require("agora-access-token");
@@ -11,18 +11,16 @@ router.post('/auth/register', register);
 router.post('/auth/login', login);
 router.get('/auth/getUser', userMiddleware(), (req, res) => getUser(req, res));
 
-router.post('/auth/addFiles', userMiddleware(), addFiles);
-
 router.post('/auth/addLanguages',userMiddleware(), addLanguages);
 router.get('/auth/getLanguages', userMiddleware(),getLanguages );
 
 router.get('/auth/getFilesbyId', userMiddleware(), getFilesbyId);
 router.get('/auth/getFilesbyUserId', userMiddleware(), getFilesbyUserId);
 
+router.post('/auth/addFiles', userMiddleware(), addFiles);
 router.put('/auth/updateFile', userMiddleware(), updateFile);
 router.delete('/auth/removeFile',userMiddleware(),  removeFile);
 router.put('/auth/updateFileName', userMiddleware(), updateFileName);
-
 
 router.put('/auth/updateUser', userMiddleware(), updateUser);
 router.delete('/auth/removeUser', userMiddleware(), removeUser);
