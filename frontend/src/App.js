eb7e6298f934a921d39168a60be547e948f34f92
mohamad-tahan <mohamadtahan880@gmail.com>
2022-07-30 22:10:29 +0300

    Frontend: Added routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,17 @@ import "./App.css"
 import { BrowserRouter, Routes, Route} from "react-router-dom";
 import Login from "./components/pages/LoginPage/Login";
 import { Toaster } from 'react-hot-toast';
-import Navbar from "./components/pages/MainPage/Navbar";
 import Home from "./components/pages/MainPage/Home.js";
 import GenerateRoomId from "./components/pages/Room/GenerateRoomId";
 import RemoteSocket from "./components/pages/Room/RemoteSocket";
+import Profile from "./components/pages/Profile/Profile";
+import AddLanguage from "./components/pages/Admin/AddLanguage";
+import AddUser from "./components/pages/Admin/AddUser";
+import ViewUsers from "./components/pages/Admin/ViewUsers";
+import ContactUs from "./components/pages/ContactUs/ContactUs";
+import ViewContacts from "./components/pages/Admin/ViewContacts";
+
+
 
 function App() {
   return (
@@ -18,17 +25,18 @@ function App() {
         <Routes>
 
         <Route path="/login" element={<Login />}></Route>
-        <Route path="/navbar" element={<Navbar />}></Route>
         <Route path="/" element={<Home />}></Route>
         <Route path="/remote/:roomId" element={<RemoteSocket />}></Route>
         <Route path="/generateRoomId" element={<GenerateRoomId />}></Route>
-
-
-
+        <Route path="/profile" element={<Profile />}></Route>
+        <Route path="/contactUs" element={<ContactUs />}></Route>
+        <Route path="/admin/addLanguage" element={<AddLanguage />}></Route>
+        <Route path="/admin/addUser" element={<AddUser />}></Route>
+        <Route path="/admin/viewUsers" element={<ViewUsers />}></Route>
+        <Route path="/admin/viewContacts" element={<ViewContacts />}></Route>
 
 
         
-
         </Routes>
 
         </div>
