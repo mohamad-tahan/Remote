6297ca45a6402e790cbef94976608feaa3bb1436
mohamad-tahan <mohamadtahan880@gmail.com>
2022-07-30 13:18:06 +0300

    Backend: Added addContactUs function

---
+++ backend/src/contactUs/controller/contact.js
@@ -0,0 +1,46 @@
+const ContactUs = require("../../../model/ContactUs");
+
+async function addContact(body) {
+    const {
+      name,
+      email,
+      phone,
+      message,
+      
+    } = body;
+  
+    const contact = new ContactUs({
+      name,
+      email,
+      phone,
+      message,
+    });
+  
+    return await contact.save();
+  }
+
+
+  async function addContactUs(req, res) {
+    try {
+      const contact = await addContact(req.body);
+      return res.send({ contact });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+
+    async function getContactUs(req, res) {
+        try {
+            const contact = await ContactUs.find();
+            return res.send(contact);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+
+    module.exports = {
+        addContactUs,
+        getContactUs
+    }
