a51de35132a532bf406cbda304a3a6e6d2048544
mohamad-tahan <mohamadtahan880@gmail.com>
2022-08-01 02:11:25 +0300

    Frontend: Added delete messages

--- frontend/src/components/pages/Admin/ViewContacts.js
+++ frontend/src/components/pages/Admin/ViewContacts.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import toast from 'react-hot-toast';
 import AdminNav from './AdminNav';
+import { MdDeleteForever } from "react-icons/md";
+import SearchBar from './SearchBar';
+
 function ViewContacts() {
     const[contacts, setContacts] = useState([]);
  
@@ -26,14 +29,38 @@ function ViewContacts() {
         getContacts();
         }, []);
 
-
+        const handleDelete = async (contactId) => {
+          const res = await fetch(
+            "/auth/deleteContactUs/?id=" + contactId,
+            {
+              method: "DELETE",
+              headers: {
+                "Content-Type": "application/json",
+                token: token,
+              },
+            }
+          )
+            .then((res) => res.json())
+            .then((res) => {
+              console.log(res);
+              if (res) {
+                toast.success("Message Removed");
+               getContacts();
+              }})
+            .catch((err) => {
+              console.log(err);
+            });
+        }
+          
+        
+      
 
 
   return (
     <div>
         <AdminNav/>
 <h1 className="viewUsers">Messages</h1>
-
+   
     <table className="table">
     <thead>
         <tr>
@@ -50,7 +77,8 @@ function ViewContacts() {
                 <td>{contact.name}</td>
                 <td>{contact.email}</td>
                 <td>{contact.phone}</td>
-                <td>{contact.message}</td>      
+                <td>{contact.message}</td>
+                <td onClick={(e)=>handleDelete(contact._id)}><MdDeleteForever style={{ color:"red", cursor:"pointer"}} /></td>      
             </tr>
             
         ))}
@@ -63,78 +91,8 @@ function ViewContacts() {
  
 
   )
-}
+        }
 
 export default ViewContacts
 
 
-// function ViewUsers() {
-
-//     const[contacts, setContacts] = useState([]);
- 
-
-//     const token = localStorage.getItem("token");
-
-//     const getContacts = async () => {
-//         const res = await fetch("/auth/getUser", {
-//           headers: { "content-type": "application/json", token: token },
-//         });
-//         const data = await res.json();
-//         console.log(data);
-    
-//         if (data) {
-//           setContacts(data);
-//           console.log(data);
-//         } else {
-//           toast.error("Something went wrong.");
-//         }
-//       };
-
-//       useEffect(() => {
-//         getContacts();
-//         }, []);
-
-
-
-
-
-
-
-//   return (
-//     <div>
-//         <AdminNav/>
-// <h1 className="viewUsers">View All Users</h1>
-
-//     <table className="table">
-//     <thead>
-//         <tr>
-//             <th>Name</th>
-//             <th>Email</th>
-//             <th>Phone</th>
-//             <th>Message</th>
-          
-//         </tr>
-//     </thead>
-//     <tbody>
-//         {/* {users.map((user) => (
-//             <tr key={user._id}>
-//                 <td>{user.name}</td>
-//                 <td>{user.username}</td>
-//                 <td>{user.email}</td>
-//                 <td>{user.user_type}</td>
-//                 <td>No</td>
-//             </tr>
-            
-//         ))} */}
-//     </tbody>
-//     </table>
-
-
-
-//     </div>
- 
-
-//   )
-// }
-
-// export default ViewUsers
